f3ddb4547d890bc8faa9b064b423c1008950c15c
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2025-04-14 21:28:01 +0200

    kh

--- TierheimAppMitQQlite/index.js
+++ TierheimAppMitQQlite/index.js
@@ -83,20 +83,37 @@ app.post("/createNewTier", (req, res) => {
   res.status(201).send("Tier wurde erfolgreich hinzugefügt");
 });
 
-//app.put("/tiere/:id")
+app.put("/tiere/:id", (req, res) => {
+  const id = req.params.id;
+  const { tierart, name, krankheit, age, gewicht } = req.body;
+  db.run(
+    `UPDATE tiere
+SET tierart = ?, name = ?, krankheit = ?, age = ?, gewicht = ?
+WHERE id = ?`,
 
-app.delete("/tiere/:id", (req, res) => {
-    const id = req.params.id;
-    const selectTiereByQuery = "DELETE  FROM  tiere  WHERE id = ?";
-    db.get(selectTiereByQuery, [id], (err, rows) => {
+    [tierart, name, krankheit, age, gewicht, id],
+    (err, rows) => {
       if (err) {
         res.status(404).send("Fehler in deiner Query Anfrage");
       } else {
-        res.json(rows);
+        res.status(201).send("Tier wurde erfolgreich hinzugefügt");
       }
-    });
+    }
+  );
+});
+
+app.delete("/tiere/:id", (req, res) => {
+  const id = req.params.id;
+  const selectTiereByQuery = "DELETE  FROM  tiere  WHERE id = ?";
+  db.run(selectTiereByQuery, [id], (err, rows) => {
+    if (err) {
+      res.status(404).send("Fehler in deiner Query Anfrage");
+    } else {
+      res.json(rows);
+    }
   });
-  res.status(201).send("Tier wurde erfolgreich hinzugefügt");
+
+  res.status(201).send("Tier wurde erfolgreich gelöscht");
 });
 
 app.listen(3000);
